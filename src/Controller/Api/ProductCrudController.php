8fa03f6066e5f2fb7cd770cf47d14db36eefecc5
Dickens Zihua Fan <dickensdjj@gmail.com>
2020-06-14 01:15:41 +1000

    - adding CRUD api routers and controller, adding homepage, product listing and product detail page templates, routers and controllers

---
+++ src/Controller/Api/ProductCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ProductCrudController extends AbstractController
+{
+    public function create(): JsonResponse
+    {
+        // TODO: new
+    }
+
+    public function update(): JsonResponse
+    {
+        // TODO: update
+    }
+
+    public function get_product(): JsonResponse
+    {
+        // TODO: get
+    }
+
+    public function get_products(): JsonResponse
+    {
+        // TODO: get all
+    }
+
+    public function delete(): JsonResponse
+    {
+        // TODO: delete
+    }
+}
